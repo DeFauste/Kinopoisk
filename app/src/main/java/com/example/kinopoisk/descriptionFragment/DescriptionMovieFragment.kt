c86d06710cc473abcbdec8970fe9bbfc5b0603c0
DeFauste <39949940+DeFauste@users.noreply.github.com>
2023-03-05 22:03:10 +0300

    add: description layout

--- app/src/main/java/com/example/kinopoisk/descriptionFragment/DescriptionMovieFragment.kt
+++ app/src/main/java/com/example/kinopoisk/descriptionFragment/DescriptionMovieFragment.kt
@@ -1,10 +1,11 @@
-package com.example.kinopoisk
+package com.example.kinopoisk.descriptionFragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.kinopoisk.R
 
 
 class DescriptionMovieFragment : Fragment() {
