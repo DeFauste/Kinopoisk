f6622d53fff79b2e9f321919e79682d81cc479d3
DeFauste <39949940+DeFauste@users.noreply.github.com>
2023-03-09 12:56:51 +0300

    add: tab layout MainFragment.kt

--- app/src/main/java/com/example/kinopoisk/mainFragment/MainFragment.kt
+++ app/src/main/java/com/example/kinopoisk/mainFragment/MainFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.kinopoisk.R
+import com.example.kinopoisk.databinding.ActivityMainBinding
+import com.example.kinopoisk.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -17,6 +24,28 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tabLayoutMain.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(requireContext(), "Tab = ${tab?.position}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
